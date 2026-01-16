9cd98986b9ef0f25fbbb1d4484e8229c91d42cb8
arnob550 <abrarshahrierarnab@gmail.com>
2026-01-16 21:56:07 +0600

    L1: Variables & DataTypes

---
+++ f001.js
@@ -0,0 +1,62 @@
+alert("Bangladesh"); // to show alert box with message "Bangladesh"
+console.log("Bangladesh"); // to log message "Bangladesh" to the console
+
+//01
+fname="Adam Gilchrist"
+console.log(fname); // Adam Gilchrist
+
+//02
+j_number=18
+console.log(j_number); // 18
+//03
+is_active=true
+console.log(is_active); // true
+
+//04
+var country="Australia"
+console.log(country); // Australia
+
+//05
+let city="Melbourne"
+console.log(city); // Melbourne 
+//06
+const pi=3.14
+console.log(pi); // 3.14    
+//07
+var score=95.5
+console.log(score);
+// 95.5
+//08
+let is_logged_in=false
+console.log(is_logged_in); // false
+//09
+const language="JavaScript"
+console.log(language); // JavaScript
+
+//Practice Problem 1
+// Create a const Object named "Product" to store information of a pen.
+
+const Product = {
+    name: "Pen",
+    brand: "Parker",
+    rating: 4.5,
+    price: 50,
+    inStock: true,
+    isOffer: false
+};
+
+console.log(Product);
+// Output the Product object to the console
+
+// Practice Problem 2
+// Create a const Profile Object
+const Profile = {
+    fullName: "Alice Johnson",
+    userName: "alicej",
+    follwers: 1555,
+    following: 890,
+    isVerified: true
+};
+
+console.log(Profile);
+// Output the Profile object to the console
